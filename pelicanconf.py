7e6d26b3cc7fdc445fa9da5f028d018c9cd5969b
h1r03 <8764683+h1r03@users.noreply.github.com>
2018-04-26 23:55:57 -0700

    Push again for testing

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,7 +18,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+#
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
