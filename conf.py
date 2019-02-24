77915fa652ce15e8af69408d56b735925939275e
h1ros <8764683+h1ros@users.noreply.github.com>
2019-02-23 16:29:14 -0800

    Update about.md

--- conf.py
+++ conf.py
@@ -136,7 +136,7 @@ TRANSLATIONS_PATTERN = '{path}.{lang}.{ext}'
 NAVIGATION_LINKS = {
     DEFAULT_LANG: (
         ("/", "Posts"),
-        ("/posts/about", "About"),
+        ("/posts/about.md", "About"),
         ("/archive.html", "Archive"),
         ("/categories/", "Tags"),
         ("/rss.xml", "RSS feed")
