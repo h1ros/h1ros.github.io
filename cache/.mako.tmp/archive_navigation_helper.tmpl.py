a53ff511bcf58414625ba629d9f44bb5cdae1fae
h1ros <data.h1ros@gmail.com>
2019-02-24 22:19:09 -0800

    Nikola auto commit.
    Nikola version: 8.0.2

--- cache/.mako.tmp/archive_navigation_helper.tmpl.py
+++ cache/.mako.tmp/archive_navigation_helper.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1551075513.410731
+_modified_time = 1551075548.45154
 _enable_loop = True
 _template_filename = '/Users/hiro/anaconda3/envs/py367/lib/python3.6/site-packages/nikola/data/themes/base/templates/archive_navigation_helper.tmpl'
 _template_uri = 'archive_navigation_helper.tmpl'
@@ -31,8 +31,8 @@ def render_archive_navigation(context):
         pagekind = context.get('pagekind', UNDEFINED)
         previous_archive = context.get('previous_archive', UNDEFINED)
         next_archive = context.get('next_archive', UNDEFINED)
-        up_archive = context.get('up_archive', UNDEFINED)
         messages = context.get('messages', UNDEFINED)
+        up_archive = context.get('up_archive', UNDEFINED)
         has_archive_navigation = context.get('has_archive_navigation', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
