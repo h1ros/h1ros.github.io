de9d62bb9501862db493de8757d2ddb3be7cbbc0
h1ros <data.h1ros@gmail.com>
2019-02-28 13:29:43 -0800

    Nikola auto commit.
    Nikola version: 8.0.1

--- cache/.mako.tmp/tag.tmpl.py
+++ cache/.mako.tmp/tag.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1551299944.0033898
+_modified_time = 1551389382.1477902
 _enable_loop = True
 _template_filename = '/Users/212339410/anaconda3/envs/py367/lib/python3.6/site-packages/nikola/data/themes/base/templates/tag.tmpl'
 _template_uri = 'tag.tmpl'
@@ -34,16 +34,16 @@ def render_body(context,**pageargs):
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
         def content():
             return render_content(context._locals(__M_locals))
-        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
-        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
-        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
         description = _import_ns.get('description', context.get('description', UNDEFINED))
-        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
         date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
-        title = _import_ns.get('title', context.get('title', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
+        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
         feeds_translations = _mako_get_namespace(context, 'feeds_translations')
+        title = _import_ns.get('title', context.get('title', UNDEFINED))
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n')
@@ -68,11 +68,11 @@ def render_extra_head(context,**pageargs):
     try:
         _import_ns = {}
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
-        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
-        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
-        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         def extra_head():
             return render_extra_head(context)
+        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         __M_writer = context.writer()
         __M_writer('\n    ')
         __M_writer(str(feeds_translations.head(tag, kind, rss_override=False)))
@@ -89,14 +89,14 @@ def render_content(context,**pageargs):
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
         def content():
             return render_content(context)
-        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
-        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
-        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
         description = _import_ns.get('description', context.get('description', UNDEFINED))
-        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
         date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
-        title = _import_ns.get('title', context.get('title', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        subcategories = _import_ns.get('subcategories', context.get('subcategories', UNDEFINED))
         feeds_translations = _mako_get_namespace(context, 'feeds_translations')
+        title = _import_ns.get('title', context.get('title', UNDEFINED))
+        tag = _import_ns.get('tag', context.get('tag', UNDEFINED))
         posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n<article class="tagpage">\n    <header>\n        <h1>')
---
+++ output/posts/be-true-tortoise/index.md
@@ -0,0 +1,66 @@
+<!--
+.. title: Be true tortoise
+.. slug: be-true-tortoise
+.. date: 2019-02-28 13:26:32 UTC-08:00
+.. tags: 
+.. category: 
+.. link: 
+.. description: 
+.. type: text
+.. status: draft
+-->
+
+### Introduction
+
+The rabbit and the tortoise. What kind of story do you imagine? —Yes, the race. Who won the race?—The tortoise. What is the lesson from this fable?—Yea, one of the typical lesson is if you work diligently, you will win. My grandfather told me a slight different lesson. 
+
+> The reason why the rabbit lost is because the rabbit looks at the tortoise. The reason why the tortoise win is because he looks at his own goal. 
+
+Young little Hiro said "I got it. I completely undestand it." 
+
+25 years later.... After all he did not understand it yet. 
+
+
+
+### Failure & Overcome
+
+Today, I have two things to share with you: one is 1 year of my failure of applying this lesson in 2018, and another is about how I am overcoming this failure now in 2019. 
+
+At the end of 2017, I keenly felt that I needed to create a technical blog or some sites to share and publish my technical ideas since I read a lot of technical blogs and tutorials in my daily job as a data scientist, and thought this was essential skill for us. One day I talked to my wife and she was also interested in hosting her own blog. So we excitedly researched about rental servers, domains, and blogging tools, all that kind of stuff. We together started to host each of blog. 
+
+54 versus 4
+
+At the end of 2018, my wife successfully hosted two blogs and made 54 posts in total. On the other hand, I only had 4 half-baked posts, which I cannot stand for. 
+
+I did not give it up yet. At the beginning of this year, I happened to see one service called, ***200 Word A day***. It is very simple platform for those who wants to build writing habits. This platform has two simple functions: 	
+
+* word counter: when you write a post, it shows how many words left till 200 words
+* streaks counter: which indicates how many consecutive days you have made your post. 
+
+I started at Jan 3rd as a part of new year resolution, and broke the streak on Jan 5th. However, I still did not give it up. Now (Feb 20, 2019), I have **44 STREAKS.**  
+
+
+
+### Lessons
+
+Even to get there, it was not that straightforward. I had difficulty to continue sometimes. But Let me share 3 lessons I learned through this experience. 
+
+- start small
+
+  In the beginning, I set too high bar for myself. That is why I could not write and continue to work.
+
+  
+
+- One day I got the very interesting comment. "compare yourself who you were yesterday"
+
+  I did not understand why I received it since I re-read my post. My writing revealed my thought habit to compare myself with others in that post. I could not continue to write a blog in 2018 since I kept comparing myself with my wife, or other successful bloggers.  
+
+- There is one more interesting comment I got. 
+  As a part of preparation for a technical blog, I tried to explain a technical concept with plain terms, which called Feynman Technique. I got the feedback for the post I made some efforts to explain Bayes Theorem, which is statistical therem. "still don't get it." After a bitter feelling, I was getting to appreciate his reading even if he did not get it. With the mindset "compare yourself with who you were", I was not scared of being judged or even critisized.  
+
+
+
+In summary, if you would like to start to build new skills, and new habit, start small. Compare yourself with who you were yesterday. Do not compare with others who already ahead of you. If you do that, you should be robust and not afraid of being judged.  
+
+Don't be a rabbit. Be true tortoise!
+
